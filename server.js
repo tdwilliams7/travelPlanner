e961b4eaa94165fe0cbc847af403f4f503c8ea12
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2019-01-22 16:11:21 -0500

    express set up with morgan cors and helmet

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const logger = require("morgan");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(helmet());
+app.use(logger("combined"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
