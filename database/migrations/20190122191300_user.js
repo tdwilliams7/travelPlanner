7c65544f695bd942baf51900a64c92222eb0f85e
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2019-01-23 17:16:09 -0500

    make email unique. users add and get working

--- database/migrations/20190122191300_user.js
+++ database/migrations/20190122191300_user.js
@@ -3,7 +3,10 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", table => {
     table.string("id").default(uuid());
     table.string("name", 255).notNullable();
-    table.string("email", 255).notNullable();
+    table
+      .string("email", 255)
+      .notNullable()
+      .unique(true);
     // table.string("password", 255).notNullable();
     table.timestamp("created_at").default(knex.fn.now());
   });
