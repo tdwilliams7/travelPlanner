0b79d268b3322b5c9cf22610ede43475bd3bd002
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2019-01-23 20:36:17 -0500

    remove material ui. create appbar and signup for skeletons

---
+++ client/src/components/SignUpForm/SignUpForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class SignUpForm extends Component {
+  state = {
+    name: "",
+    email: ""
+  };
+
+  render() {
+    return (
+      <div container spacing={0}>
+        <div>SignUpForm</div>
+      </div>
+    );
+  }
+}
+
+export default SignUpForm;
